8a979bc47ef869306c4de64854513d51b6282f92
Edward Ji <jiziao6@gmail.com>
2021-11-04 15:12:34 +1100

    Add game object manager tests full coverage

---
+++ src/test/java/demolition/GameObjectManagerTest.java
@@ -0,0 +1,21 @@
+package demolition;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameObjectManagerTest {
+
+    // Test that enemy objects can be destoryed by explosions.
+    @Test
+    public void inFieldTest() {
+        GameObjectManager manager = new GameObjectManager();
+
+        assertTrue(manager.inField(7, 6));
+        assertFalse(manager.inField(7, -1));
+        assertFalse(manager.inField(7, 13));
+        assertFalse(manager.inField(-1, 6));
+        assertFalse(manager.inField(15, 6));
+        assertFalse(manager.inField(-1, -1));
+        assertFalse(manager.inField(15, 13));
+    }
+}
