f70dc22cb8d5e77a5da9c41d49e0759493c41f65
Edward Ji <jiziao6@gmail.com>
2021-11-04 21:08:23 +1100

    Add animated object move outside field test

--- src/test/java/demolition/AnimatedGameObjectTest.java
+++ src/test/java/demolition/AnimatedGameObjectTest.java
@@ -5,9 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AnimatedGameObjectTest {
 
+    // Test animated objects can not move outside map.
     @Test
-    public void getLoaderTest() {
-        App app = new App();
-        assertNull(app.getLoader());
+    public void moveUnboundedTest() {
+        final String unboundedConfigPath = "src/test/config/config_unbounded_level.json";
+
+        App testApp = AppTest.testApp(unboundedConfigPath);
+
+        // Attempts to move player outside the grid.
+        testApp.player.control(Direction.LEFT);
+        testApp.player.control(Direction.UP);
+
+        // Player shouldn't have moved.
+        assertEquals(0, testApp.player.getGridX());
+        assertEquals(0, testApp.player.getGridY());
+        assertEquals(Direction.DOWN, testApp.player.getDirection());
     }
 }
